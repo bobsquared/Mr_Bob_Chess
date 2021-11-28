38c3c94c25cafe326486e82c801bd8d3ae412aeb
bobsquared <vyu189@gmail.com>
2021-11-28 14:31:02 -0800

    bump version

--- src/uci.h
+++ src/uci.h
@@ -18,7 +18,7 @@ public:
 private:
 
     const std::string NAME = "Mr Bob";
-    const std::string VERSION = "v1.0.0";
+    const std::string VERSION = "v1.1.0";
     const std::string AUTHOR = "Vincent Yu";
 
 };
