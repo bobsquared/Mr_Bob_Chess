d756bbcf0bd6d4a017c97341d0614269f644f882
bobsquared <vyu189@gmail.com>
2023-02-01 00:15:02 -0800

    clean main.cpp

--- src/nnue/main.cpp
+++ src/nnue/main.cpp
@@ -8,12 +8,6 @@ int main(int argc, char **argv) {
     InitLateMoveArray();
 
     if (argc == 3) {
-        #ifdef KPTrainer
-        Trainer trainer = Trainer();
-        std::string infile = argv[1];
-        std::string outfile = argv[2];
-        trainer.extractFile(infile, outfile);
-        #else
         Bitboard board = Bitboard();
         std::string network = argv[1];
         std::string fen = argv[2];
@@ -21,17 +15,6 @@ int main(int argc, char **argv) {
         int ret = model.evaluate(fen, board);
 
         std::cout << "Score: " << ret << std::endl;
-        #endif
-    }
-    else {
-        Trainer trainer = Trainer();
-        trainer.getFile("NNUE_d8_70M_r.epd");
-        // trainer.getFileSigmoid("../texeldata.fen");
-        // KPNNUE model = KPNNUE("networks/test25/dog_47.bin");
-        int modelSize[45] = {768, 256, 8, 1};
-        KPNNUE model = KPNNUE(3, modelSize);
-        trainer.train(model, "networks/test26/dog");
-    }
-    
+    } 
 
 }
