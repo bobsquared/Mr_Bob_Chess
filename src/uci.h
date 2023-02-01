fd26e37a631341932db32dfea8f8cbffefdf9258
bobsquared <vyu189@gmail.com>
2023-01-31 17:47:34 -0800

    Change to 1.2.0

--- src/uci.h
+++ src/uci.h
@@ -21,7 +21,7 @@ public:
 private:
 
     const std::string NAME = "Mr Bob";
-    const std::string VERSION = "v1.1.0";
+    const std::string VERSION = "v1.2.0";
     const std::string AUTHOR = "Vincent Yu";
     const std::string DEFAULT_NETWORK = "nets/bob_brain-012022e141.nnue";
 
