f57f4b9d27d2b80cdfe91e1901923b9d78ebe966
bobsquared <vyu189@gmail.com>
2023-09-21 22:53:55 -0700

    nmp R reduction change

--- src/search.cpp
+++ src/search.cpp
@@ -432,7 +432,7 @@ int Search::pvSearch(Bitboard &b, ThreadSearch *th, int depth, int alpha, int be
 
         // Null move pruning
         if (canNullMove && staticEval >= beta && depth >= 2 && th->nullMoveTree && b.nullMoveable()) {
-            int R = 3 + depth / (7 - improving) + std::min((staticEval - beta) / 300, 3);
+            int R = 3 + depth / 6 + std::min((staticEval - beta) / 300, 3);
             th->searchStack[ply + 1].extLevel = extLevel;
 
             b.make_null_move();
