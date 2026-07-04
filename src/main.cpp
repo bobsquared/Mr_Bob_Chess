8e0a9bcdf00442ba7b5b9e217fedfd7598c878bd
bob <vyu189@gmail.com>
2026-07-03 18:01:02 -0700

    threads can go up to 2048 now

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ int main(int argc, char* argv[]) {
     CommandInterface* c = nullptr;
     
     options.addOption([&](int n){TT::setSize(n);}, "Hash", 256, 1, 131072);
-    options.addOption([&](int n){THREAD::setNThreads(n);}, "Threads", 1, 1, 256);
+    options.addOption([&](int n){THREAD::setNThreads(n);}, "Threads", 1, 1, 2048);
     options.addOption([&](int n){s.setMultiPVSearch(n);}, "MultiPV", 1, 1, 256);
 
     options.addOption([&](int n){s.setRFPsearch(n);}, "rfp", 64, 1, 500);
