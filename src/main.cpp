08ea192e069580ebfc286dc115dd63148f3a5ae8
bob <vyu189@gmail.com>
2026-07-25 01:46:22 -0700

    updated hash size

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char* argv[]) {
 
     CommandInterface* c = nullptr;
     
-    options.addOption([&](int n){TT::setSize(n);}, "Hash", 256, 1, 131072);
+    options.addOption([&](int n){TT::setSize(n);}, "Hash", 256, 1, 524288);
     options.addOption([&](int n){THREAD::setNThreads(n);}, "Threads", 1, 1, 2048);
     options.addOption([&](int n){s.setMultiPVSearch(n);}, "MultiPV", 1, 1, 256);
 
