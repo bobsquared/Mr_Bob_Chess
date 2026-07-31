99843d1015da6774c90b8bbb12242bebb2de27f1
bob <vyu189@gmail.com>
2026-07-31 07:41:28 -0700

    nmp tuning

--- src/search.cpp
+++ src/search.cpp
@@ -379,10 +379,9 @@ int Search::pvSearch(Board &b, ThreadData &td, int depth, int alpha, int beta, b
         }
 
         // Null move pruning
-        if (canNullMove && staticEval >= beta + 25 * (phase >= 200) + 128 - depth * 4 && depth >= 2 + !hashed 
-                        && td.nullMoveTree && BITBOARD::nullMoveable(b.state) 
-                        && (!hashed || hashedBoard.score >= beta)) {
-            int R = 3 + depth / 3 + std::min((staticEval - beta) / 300, 4);
+        if (canNullMove && staticEval >= beta + 25 * (phase >= 200) + 128 - depth * 4 && depth >= 2
+                        && td.nullMoveTree && BITBOARD::nullMoveable(b.state)) {
+            int R = 3 + depth / 3 + std::max(0, std::min((staticEval - beta) / 200, 4));
             td.searchStack[ply + 1].extLevel = extLevel;
             BITBOARD::make_null_move(b);
             int nullRet = -pvSearch(b, td, depth - R - 1, -beta, -beta + 1, false, ply + 1, false);
